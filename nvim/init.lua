99f17f993c58477cbbc2d9553afc607e6416819e
Olli-Pekka Nikka <ollipekkanikka@gmail.com>
2023-02-08 21:14:43 +0200

    add custom configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,15 @@
-require("stidi")
+require("stidi.init")
+require("stidi.remap")
+require("stidi.plugins")
+require("stidi.base")
+
+local has = vim.fn.has
+local is_mac = has "macunix"
+local is_win = has "win32"
+
+if is_mac then
+  require('stidi.macos')
+end
+if is_win then
+  require('stidi.windows')
+end
